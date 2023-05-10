9ff53ad5675c23bb25ba69ef5b7a348d13a5e0e8
aspahn99 <37812253+aspahn99@users.noreply.github.com>
2023-05-10 13:40:54 -0400

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,32 +1,28 @@
 import streamlit as st                  # pip install streamlit
 
-st.markdown("# Practical Applications of Machine Learning (PAML)")
+st.markdown("# Practical Applications of Machine Learning (PAML) Final Project")
 
 #############################################
 
-st.markdown("### Homework 3 - Predicting Product Review Sentiment Using Classification")
+st.markdown("### Classifying Fake News and Predicting News Sentiment")
 
 #############################################
 
-st.markdown("Welcome to the Practical Applications in Machine Learning (PAML) Course! You will build a series of end-to-end ML pipelines, working with various data types and formats, and will need to engineer your system to support training, testing, and deploying ML models.")
+st.markdown("Project by Annie Spahn (as2757), Emma Condie (erc97), Lauren Tran (lat89)")
 
-st.markdown("""The goal of this assignment is to build a classification machine learning (ML) pipeline in a web application to use as a tool to analyze the models to gain useful insights about model performance. Using trained classification models, build a ML application that predicts whether a product review is positive or negative.
-
-The learning outcomes for this assignment are:
-- Build end-to-end classification pipeline with four classifiers 1) Logistic Regression, 2) Stochastic Gradient Descent, 3) Stochastic Gradient Descent with Cross Validation, and 4) Majority Class.
-- Evaluate classification methods using standard metrics including precision, recall, and accuracy, ROC Curves, and area under the curve.
-- Develop a web application that walks users through steps of the classification pipeline and provide tools to analyze multiple methods across multiple metrics. 
-- Develop a web application that classifies products as positive or negative and indicates the cost of displaying false positives and false negatives using a specified model.
+st.markdown("""By creating an application that predicts whether articles are real or fake, we aim to help news readers be more informed about current events and to increase trust in reputable news sources.
 """)
 
-st.markdown(""" Amazon Products Dataset
+st.markdown(""" Fake and Real News Dataset
 
-This assignment involves training and evaluating ML end-to-end pipeline in a web application using the Amazon Product Reviews dataset. Millions of Amazon customers have contributed over a hundred million reviews to express opinions and describe their experiences regarding products on the Amazon.com website. This makes Amazon Customer Reviews a rich source of information for academic researchers in the fields of Natural Language Processing (NLP), Information Retrieval (IR), and Machine Learning (ML), amongst others. Specifically, this dataset was constructed to represent a sample of customer evaluations and opinions, variation in the perception of a product across geographical regions, and promotional intent or bias in reviews.
+This final project involves training and evaluating ML end-to-end pipeline in a web application using the Fake and Real News Dataset dataset from Kaggle. 
 
-We have added additional features to the dataset. There are many features, but the important ones include:
-- name: name of Amazon product	
-- reviews.text: text in review	
-- reviews.title: title of reviews	
+The features in this dataset are:
+- title: title of the article 
+- text: text in article
+- subject: subject of the article 
+- date: date of when the article was published
+- label: 1 if true article, 0 if fake news
 """)
 
 st.markdown("Click **Explore Dataset** to get started.")
